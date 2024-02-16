3e4190012efc9604b49be2e0da6d42edda6531b7
Amy Wong <akwong24@outlook.com>
2024-02-16 00:56:29 -0800

    Updated timer interface

--- src/components/TimeLeft.tsx
+++ src/components/TimeLeft.tsx
@@ -8,9 +8,13 @@ momentDurationFormatSetup(moment);
 const TimeLeft: React.FC<Props> = ({ timerLabel, handleStartStopClick, startStopButtonLabel, timeLeft, handleResetButtonClick }) => {
     const formatTimeLeft = moment.duration(timeLeft, 's').format('mm:ss', { trim: false });
     return (
-        <div className="timeLeft-container">
-            <p id="timer-label">{timerLabel}</p>
-            <p>{formatTimeLeft}</p>
+        <div className="timeLeft-container animatable">
+            <svg>
+                <circle cx="50%" cy="50%" r="150" />
+                <circle cx="50%" cy="50%" r="150" pathLength="1" />
+                <text x="250" y="250" text-anchor="middle" id="timer-label">{timerLabel}</text>
+                <text x="250" y="170" text-anchor="middle"><tspan id="timer-time">{formatTimeLeft}</tspan></text>
+            </svg>
             <div className="button-container">
                 <button id="start-stop-button" onClick={handleStartStopClick}><span>{startStopButtonLabel}</span></button>
                 <button id="reset-button" onClick={handleResetButtonClick}><span>Reset</span></button>
